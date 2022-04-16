04b42b2ab08f8069a24d43945dacdaa9acfd0edd
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-16 19:52:46 -0400

    adding html and js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//locate our submit event and its value
+const handleSubmit = function() {
+  const form = document.querySelector('form')
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const newTask = e.target[0].value
+    const list = document.querySelector('#tasks')
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+    const ddl = document.createElement('SELECT')
+    const opt1 = document.createElement('option')
+    const opt2 = document.createElement('option')
+    li.textContent = newTask
+    btn.textContent = " X"
+    opt1.text = "High"
+    opt2.text = "Medium"
+    ddl.add(opt1)
+    ddl.add(opt2)
+    btn.className = "button"
+    li.append(btn, ddl)
+    btn.addEventListener('click', () => {
+      li.remove()
+    })
+    list.append(li)
+    form.reset()
+  })
+}
+
+handleSubmit()
+
